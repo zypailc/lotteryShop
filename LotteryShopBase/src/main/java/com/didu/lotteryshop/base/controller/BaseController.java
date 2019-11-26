a0bc6689634b2f429f9772795f93ba83ced02cea
187971957@qq.com <187971957>
2019-11-26 10:50:16 +0800

    1.统一管理父级controller

--- LotteryShopBase/src/main/java/com/didu/lotteryshop/base/controller/BaseController.java
+++ LotteryShopBase/src/main/java/com/didu/lotteryshop/base/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.didu.lotteryshop.base.controller;
 
 import com.didu.lotteryshop.common.base.contorller.BaseContorller;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class BaseController extends BaseContorller {
 }
